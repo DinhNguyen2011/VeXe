2abcc9086b2375673237f084533c764e819756d0
DinhNguyen2011 <dinhn6666@gmail.com>
2025-04-25 21:25:46 +0700

    layout user

---
+++ Asp.netWebDatVe/Asp.netWebDatVe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.netWebDatVe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
