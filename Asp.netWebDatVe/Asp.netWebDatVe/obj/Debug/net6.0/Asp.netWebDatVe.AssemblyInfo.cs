fe489ce271116eba6fbf0f63f6888aecb627ee0b
DinhNguyen2011 <dinhn6666@gmail.com>
2025-04-27 21:08:19 +0700

    new change

--- Asp.netWebDatVe/Asp.netWebDatVe/obj/Debug/net6.0/Asp.netWebDatVe.AssemblyInfo.cs
+++ Asp.netWebDatVe/Asp.netWebDatVe/obj/Debug/net6.0/Asp.netWebDatVe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.netWebDatVe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c73a422a7332f51d81fceea860773e72479880a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f3c639cec15ee61af5d4d7ae7dd8a323b178b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.netWebDatVe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.netWebDatVe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
