78a4ad96a4e7f82fdd8764dd5979523ce43a5e57
nhuocan1403 <nhuocan1403@gmail.com>
2025-07-01 23:22:18 +0700

    he he

--- Asp.netWebDatVe/Asp.netWebDatVe/obj/Debug/net6.0/Asp.netWebDatVe.AssemblyInfo.cs
+++ Asp.netWebDatVe/Asp.netWebDatVe/obj/Debug/net6.0/Asp.netWebDatVe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.netWebDatVe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d20729aefffa1d9c6979d1843e5b0745075ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eebe966dfea714a103b288f954618a905ea367f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.netWebDatVe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.netWebDatVe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
